3853ade66d6a23913fe1348a8a6218abaeb34b89
Necoe <2061919886@qq.com>
2025-10-22 19:40:16 +0800

    重新来一遍

--- routes/admin.py
+++
@@ -1,5 +0,0 @@
-from main import app
-
-@app.route('/create_article.html')
-def create_article():
-    return "create article"
--- services/article_service.py
+++
@@ -1,7 +0,0 @@
-from models.article import Article
-from routes import db
-
-
-class ArticleService:
-    def get_article(self, id):
-        return db.session.get(Article, id)
